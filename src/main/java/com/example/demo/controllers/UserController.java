5faec75888f8c557f83041c80b1f1d24366d2cbc
Zarqi Ezzoubair <98271602+regisx001@users.noreply.github.com>
2024-12-29 00:47:59 +0000

    chore: create User Application.

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entities.User;
+import com.example.demo.services.UserService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RestController
+@RequestMapping("/api/users")
+public  class UserController {
+    
+    @Autowired
+    private UserService userService;
+    
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUser();
+    }
+
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+    
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+         userService.deleteUser(id);
+    }
+}
---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,40 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Entities.User;
+import com.example.demo.repositories.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    
+    
+    public User createUser(User user){
+    return userRepository.save(user);
+    }
+
+    public List<User> getAllUser(){
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id){
+        return userRepository.findById(id).orElse(null);
+    }
+
+
+    public User updateUser(Long id,User user){
+        user.setId(id);
+        return userRepository.save(user);
+    }   
+
+    public void deleteUser(Long id){
+        userRepository.deleteById(id);
+    }
+
+
+}
